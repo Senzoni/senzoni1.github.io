82b64a053d095b2d461fad06e02e4972085fabcc
Senzoni <93424493+Senzoni@users.noreply.github.com>
2022-07-20 01:58:53 +0300

    123111

--- js/MyScript.js
+++ js/MyScript.js
@@ -2,23 +2,54 @@
 
 	$(window).scroll(( ) => {
 		let scrollDistance = $(window).scrollTop();
+
+		$('.section').each((i, el) => {
+
+			if ($(el).offset().top - $('nav').outerHeight() <= scrollDistance) {
+				$('nav a').each((i, el) => {
+					if ($(el).hasClass('active')) {
+						$(el).removeClass('active');
+					}
+				});
+
+				$('nav li:eq(' + i + ')').find('a').addClass('active'); 
+			}
+		});
 		
 
 	});
+
 	$(".loaderArea").css("display", "none");
 
 	function calculate(){
 		let sum = parseInt ($("#select1 option:selected").val()) + parseInt ($("#select2 option:selected").val()) + parseInt ($("#select3 option:selected").val());
 		let days = parseInt ($("#select1 option:selected").attr("days")) + parseInt ($("#select2 option:selected").attr("days")) + parseInt ($("#select3 option:selected").attr("days"));
-		$(".days.digit").text(days);
-		$(".price.digit").text(sum);
+		$(".days .digit").text(days);
+		$(".price .digit").text(sum);
 	}
 	
 	$("select").on("change", function(){
 		calculate();
 	}); 
 	calculate();
+
+	let options = {threshold: [0.5]};
+		let observer = new IntersectionObserver(onEntry, options);
+		let elements = $('.element-animation');
+		elements.each((i,el) => {
+			observer.observe(el);
+		});
 	});	
+
+	
+
+		function onEntry (entry){
+			entry.forEach(change => {
+				if (change.isIntersecting){
+					change.target.classList.add('show-animation');
+				}
+			});
+		}
 
 
 $('a[href^="#"]').click(function(){
